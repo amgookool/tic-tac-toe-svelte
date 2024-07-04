4ca857a2198e6b32d2bca071734b5093f12f724b
Adrian Gookool <amgookool@hotmail.com>
2024-07-04 00:38:32 -0400

    Started created menu components elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,29 @@
 <script lang="ts">
-  import TicTacToe from "@lib/TicTacToe.svelte";
-  import Menu from "@lib/Menu.svelte";
-  import "./app.css";
-  import { onMount, tick } from "svelte";
+	import TicTacToe from '@lib/TicTacToe.svelte';
+	import Menu from '@lib/Menu.svelte';
+	import './app.css';
+	import { onMount, tick } from 'svelte';
 
-  let showTicTacToe = true;
+	let showTicTacToe = true;
 
-  onMount(async () => {
-    await tick();
-    showTicTacToe = false;
-  });
+	onMount(async () => {
+		await tick();
+		showTicTacToe = false;
+	});
 </script>
 
 <main>
-  {#if showTicTacToe}
-    <div class="container mx-auto flex flex-col items-center justify-center">
-      <TicTacToe />
-      <button
-        class="btn btn-secondary"
-        on:click={() => (showTicTacToe = !showTicTacToe)}
-      >
-        Back to Main Menu</button
-      >
-    </div>
-  {:else}
-    <div class="container mx-auto flex flex-col items-center justify-center">
-      <Menu />
-      <button
-        class="btn btn-primary"
-        on:click={() => (showTicTacToe = !showTicTacToe)}
-      >
-        Start
-      </button>
-    </div>
-  {/if}
+	{#if showTicTacToe}
+		<div class=" container flex flex-col items-center justify-center">
+			<TicTacToe />
+			<button class="btn btn-secondary btn-lg btn-wide" on:click={() => (showTicTacToe = !showTicTacToe)}>
+				Back to Main Menu</button
+			>
+		</div>
+	{:else}
+		<div class="h-screen container mx-auto flex flex-col items-center justify-center">
+			<Menu />
+			<!-- <button class="btn btn-primary btn-lg btn-wide" on:click={() => (showTicTacToe = !showTicTacToe)}> Start </button> -->
+		</div>
+	{/if}
 </main>
--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,6 +1,32 @@
-<script lang="ts"></script>
+<script lang="ts">
+	let playerXName: string;
+	let playerOName: string;
+	let shouldDisableInput = false;
 
-<div>
-  <h1>Tic Tac Toe</h1>
-  
+	const savePlayerNames = () => {
+		localStorage.removeItem('players');
+		const players = {
+			playerX: playerXName,
+			playerO: playerOName,
+		};
+		localStorage.setItem('players', JSON.stringify(players));
+		shouldDisableInput = true;
+	};
+</script>
+
+<div class="card bg-neutral text-neutral-content w-60%">
+	<div class="card-body items-center text-center">
+		<h2 class="card-title">Tic Tac Toe</h2>
+		<p class="text-start">
+			Hey there, welcome to our awesome Tic Tac Toe game! 🎉 Whether you're a seasoned strategist or just here for some fun, we're
+			thrilled to have you. Pick your X or O, and let's see who can claim victory first. Remember, it's all about having a good
+			time, so play fair and enjoy every move. May the best player win! 😊
+		</p>
+		<input id="playerX" disabled={shouldDisableInput} bind:value={playerXName} type="text" placeholder="Enter Player X Name" />
+		<input id="playerO" disabled={shouldDisableInput} bind:value={playerOName} type="text" placeholder="Enter Player O Name" />
+		<div class="card-actions justify-end">
+			<button class="btn btn-secondary" disabled={shouldDisableInput} on:click={savePlayerNames}>Save</button>
+			<button class="btn btn-primary">Accept</button>
+		</div>
+	</div>
 </div>
